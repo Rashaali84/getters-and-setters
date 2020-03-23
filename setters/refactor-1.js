9457d8dd6ccefe3c45e7c904a478b74cc5d38acb
Rasha Ali <rashaali84@gmail.com>
2020-03-23 19:26:28 +0100

    Update refactor-1.js

--- setters/refactor-1.js
+++ setters/refactor-1.js
@@ -16,11 +16,11 @@ const obj2 = {
 obj1.setGreetingName('obj1');
 console.assert(obj1.greeting === "hi, I'm obj1!", 'Test 1');
 
-obj2.setGreetingName = 'obj2'; // write this line
+obj2.greetingName = 'obj2'; // write this line
 console.assert(obj2.greeting === "hi, I'm obj2!", 'Test 2');
 
 obj1.setGreetingName('hi');
 console.assert(obj1.greeting === "hi, I'm hi!", 'Test 3');
 
-obj2.setGreetingName = 'bye!';; // write this line
+obj2.greetingName = 'bye!';; // write this line
 console.assert(obj2.greeting === "hi, I'm bye!", 'Test 4');
